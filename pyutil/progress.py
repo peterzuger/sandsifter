43c80787b1730f4b4c4de0cca5030f11b261db9e
Peter Züger <zueger.peter@icloud.com>
2020-07-24 19:48:17 +0200

    run some more python-black to format the code

--- pyutil/progress.py
+++ pyutil/progress.py
@@ -3,6 +3,7 @@ import sys
 PROGRESS_BAR_WIDTH = 40
 PROGRESS_LINE_MAX = 1024
 
+
 def progress(i, n, text="", refresh=1, unknown=False):
     if not n:
         n = 1
@@ -12,16 +13,14 @@ def progress(i, n, text="", refresh=1, unknown=False):
     if i % refresh == 0 or i == n:
         c = (i * PROGRESS_BAR_WIDTH / n) % (PROGRESS_BAR_WIDTH + 1)
         if unknown:
-            bar = " " * (c - 1) + "=" * (1 if c else 0) + " " * (PROGRESS_BAR_WIDTH-c)
+            bar = " " * (c - 1) + "=" * (1 if c else 0) + " " * (PROGRESS_BAR_WIDTH - c)
             percent = ""
         else:
-            bar = "=" * c + " " * (PROGRESS_BAR_WIDTH-c)
+            bar = "=" * c + " " * (PROGRESS_BAR_WIDTH - c)
             percent = "%5.1f%%" % (float(i) * 100 / n)
-        sys.stdout.write("[%s] %s %s   " % ( \
-                bar,
-                percent,
-                "- %s" % text if text else ""
-                ))
+        sys.stdout.write(
+            "[%s] %s %s   " % (bar, percent, "- %s" % text if text else "")
+        )
         sys.stdout.flush()
         sys.stdout.write("\b" * PROGRESS_LINE_MAX)
         if not unknown and i == n:
